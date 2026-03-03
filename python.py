f1c2ec223777816ae10002f05fccbc5c6539ca94
Kayden Watson <khjw1101@gmail.com>
2026-03-03 13:19:05 +1300

    err

--- python.py
+++ python.py
@@ -1,5 +1,5 @@
 name_age_list = ["Jack",14,"Jim",16,"Jason",15]
 list_count = 0
 while list_count < len(name_age_list):
-    print(f"The name is {name_age_list[list_count]} and the age is {name_age_list[list_count + 1]}")
+    print(f"The name is {name_age_list[list_count]} and the age is {name_age_list[list_count+1]}")
     list_count +=1
