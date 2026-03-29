2c0876f948b0951ed4f6eba52664b8d82275a024
Kayden Watson <khjw1101@gmail.com>
2026-03-30 12:19:41 +1300

    Fixed bug

--- AS92004.py
+++ AS92004.py
@@ -85,10 +85,11 @@ print(f"Hello {name} you are {age}. You are going to the {activities_list_tuple[
 which is {activities_list_tuple[int(activity_selection)][3]}. Your meal is {meal_input}. You {shuttle_bus_needed} need the shuttle bus.")
 
 #Camp total cost 
+camp_cost = activities_list_tuple[int(activity_selection)][4]
 if shuttle_bus_needed == "do":
-    camp_cost = {activities_list_tuple[int(activity_selection)][4]} + shuttle_bus_needed
+    camp_cost += shuttle_bus_cost
 if shuttle_bus_needed == "don't":
-    camp_cost = {activities_list_tuple[int(activity_selection)][4]}
+    camp_cost += camp_cost
 
 #confirmation for camp 
 confirmation = ""
