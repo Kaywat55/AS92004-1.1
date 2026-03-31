4f3ad06d5c8d6d932051f483b024230eddc993b0
Kayden Watson <khjw1101@gmail.com>
2026-03-31 14:32:45 +1300

    Fixed bugs

--- AS92004.py
+++ AS92004.py
@@ -14,15 +14,15 @@ print(f"{activities_list_tuple[2][0]}        {activities_list_tuple[2][1]}      
 
 #Asking user for their name and checks if valid.
 name = ""
-while len(name) == 0 or name.isdigit() == True :
-    name = input("What is your name: ").title()
+while len(name) == 0 or name.isalpha() == False :
+    name = input("What is your name: ").title().strip()
     if name == "" : print("You must enter your name!")
-    if name.isdigit() == True : print("You must not enter numbers!")    
+    if name.isalpha() == False : print("You must not enter numbers!")    
 
 #Asking user for their age and checks if valid.
 age = ""
 while len(age) == 0 or age.isdigit() == False:
-    age = (input("What is your age: "))
+    age = (input("What is your age: ")).strip()
     if len(age) == 0 or age.isdigit() == False : print("You must enter your age!")
 
 #seeing if user meets the age requrements.
@@ -37,7 +37,7 @@ if int(age) >= camp_leader_age:
 #asks if user wants to be a camp leader and checks if vaild.
     camp_leader_option = ""
     while camp_leader_option == "" or camp_leader_option.isdigit() == True or camp_leader_option != "yes" or camp_leader_option != "no":
-        camp_leader_option = input("Do you want to be a camp leader: ").lower()
+        camp_leader_option = input("Do you want to be a camp leader: ").lower().strip()
         if camp_leader_option == "": print("You must not leave it blank!")
         if camp_leader_option.isdigit() == True: print("You must not enter numbers!")
         if camp_leader_option != "" and camp_leader_option.isdigit() == False:
@@ -51,7 +51,7 @@ if int(age) >= camp_leader_age:
 #asking the user what activity they would like to do.
 activity_selection = ""
 while activity_selection == "" or activity_selection.isdigit() == False:
-    activity_selection = input("What number camp do you want to go on: ") 
+    activity_selection = input("What number camp do you want to go on: ").strip()
     if activity_selection == "": print("You must enter something!")
     if activity_selection.isdigit() == False: print("You must enter a number!")
     if activity_selection.isdigit() == True :
@@ -60,7 +60,7 @@ while activity_selection == "" or activity_selection.isdigit() == False:
 #Asking for type of meal and checks if vaild.
 meal_input = ""
 while meal_input == "" or meal_input.isdigit() == True or meal_input not in meal_options:
-    meal_input = input(f"What meal do you want: standard, vegeterian, vegan: ").lower()
+    meal_input = input(f"What meal do you want: standard, vegeterian, vegan: ").lower().strip()
     if meal_input == "": print("You must enter something!")
     if meal_input.isdigit() == True: print("You must not enter a number!")
     if meal_input != "" and meal_input.isdigit() == False:
@@ -69,7 +69,7 @@ while meal_input == "" or meal_input.isdigit() == True or meal_input not in meal
 # Asking for Suttle bus option and checks if vaild.
 shuttle_bus_option = ""
 while shuttle_bus_option == "" or shuttle_bus_option.isdigit() == True or shuttle_bus_option != "yes" or shuttle_bus_option != "no":
-    shuttle_bus_option = input("Do you want the shuttle bus (80$) ").lower()
+    shuttle_bus_option = input("Do you want the shuttle bus (80$) ").lower().strip()
     if shuttle_bus_option == "": print("You must not leave it blank!")
     if shuttle_bus_option.isdigit() == True: print("You must not enter numbers!")
     if shuttle_bus_option != "" and shuttle_bus_option.isdigit() == False:
@@ -94,7 +94,7 @@ if shuttle_bus_needed == "don't":
 #confirmation for camp and checks if vaild.
 confirmation = ""  
 while confirmation == "" or confirmation.isdigit() == True or confirmation != "yes" or confirmation != "no":
-    confirmation = input(f"Plese comfirm your stay with a price of {camp_cost} ").lower()
+    confirmation = input(f"Plese comfirm your stay with a price of {camp_cost} ").lower().strip()
     if confirmation == "": print("You must not leave it blank!")
     if confirmation.isdigit() == True: print("You must not enter numbers!")
     if confirmation != "" and confirmation.isdigit() == False:
